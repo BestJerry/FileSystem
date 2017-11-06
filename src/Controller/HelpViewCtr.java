c0c23047e10e89466988033b287255ef461902af
Jerry <jerrywu.aiesec.scau@gmail.com>
2017-11-06 23:37:15 +0800

    2017.11.06 添加幫助界面

---
+++ src/Controller/HelpViewCtr.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import javax.naming.InitialContext;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelpViewCtr implements Initializable {
+
+    private Stage stage;
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/Controller/TopMenuCtr.java
+++ src/Controller/TopMenuCtr.java
@@ -6,13 +6,21 @@ import com.sun.javafx.robot.impl.FXRobotHelper;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -53,5 +61,26 @@ public class TopMenuCtr implements Initializable{
                 }
             }
         });
+    }
+
+    public void showHelp(MouseEvent mouseEvent) throws IOException {
+
+        Stage stage = new Stage();
+        URL location = getClass().getResource("/resources/HelpView.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(location);
+        fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+        VBox vBox = fxmlLoader.load(location.openStream());
+        HelpViewCtr helpViewCtr = fxmlLoader.getController();
+        helpViewCtr.setStage(stage);
+        stage.setTitle("°ïÖú");
+        stage.setResizable(false);
+        Scene scene = new Scene(vBox, 480, 480);
+        stage.setScene(scene);
+        stage.setAlwaysOnTop(true);
+        //ÉèÖÃÄ£Ì¬´°¿Ú
+        //stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+
     }
 }
